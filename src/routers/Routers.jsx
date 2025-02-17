a80c123a25d89eb04b68184e749a292972016c3d
devhan-hub <hanabduman@gmail.com>
2025-02-17 11:20:27 +0300

    feat(routes): set up routing for all pages

---
+++ src/routers/Routers.jsx
@@ -0,0 +1,26 @@
+import { Routes ,Route } from 'react-router-dom'
+import Shope from '../pages/Shope'
+import Cart from '../pages/Cart'
+import ProductDetails from '../pages/ProductDetails'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Home from '../pages/Home'
+import { Navigate } from 'react-router-dom'
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate to="home"/>}/>
+      <Route path='home' element={<Home/>}/>
+      <Route path='shop' element={<Shope/>}/>
+      <Route path='shop/:id' element={<ProductDetails/>}/>
+      <Route path='cart' element={<Cart/>}/>
+      <Route path='checkout' element={<Checkout/>}/>
+      <Route path='login' element={<Login/>}/>
+      <Route path='signup' element={<Signup/>}/>
+
+    </Routes>
+  )
+}
+
+export default Routers
